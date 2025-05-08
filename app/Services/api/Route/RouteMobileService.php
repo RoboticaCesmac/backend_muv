52ed2f7f95b19f284d2c035d949582cad83ee670
GustavoBarrel02 <gustavo.barrel@acesseseucondominio.com>
2025-05-08 20:23:45 -0300

    fix: ajuste de rotas com 2 por pagina.

--- app/Services/api/Route/RouteMobileService.php
+++ app/Services/api/Route/RouteMobileService.php
@@ -29,7 +29,7 @@ class RouteMobileService
         return $this->route
             ->where('user_id', $user->id)
             ->with('routePoints', 'vehicle')
-            ->paginate(1);
+            ->paginate(2);
     }
 
     /**
